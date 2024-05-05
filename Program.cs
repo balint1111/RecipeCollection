bae9049a8ffa70825994c70507d582d40077dd53
Bálint Györffy <gyorffy.balint@hellopay.hu>
2024-05-05 19:15:20 +0200

    try

--- Program.cs
+++ Program.cs
@@ -144,22 +144,22 @@ using (var scope = app.Services.CreateScope())
 }
 
 await app.Services.GetRequiredService<IUserService>().CreateAdminIfNotExist();
-string localIP = LocalIPAddress();
-
-app.Urls.Add("http://" + localIP + ":5000");
-app.Urls.Add("https://" + localIP + ":5001");
+// string localIP = LocalIPAddress();
+//
+// app.Urls.Add("http://" + localIP + ":5000");
+// app.Urls.Add("https://" + localIP + ":5001");
 
 app.Run();
 
-
-static string LocalIPAddress() {
-    using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0)) {
-        socket.Connect("8.8.8.8", 65530);
-        IPEndPoint? endPoint = socket.LocalEndPoint as IPEndPoint;
-        if (endPoint != null) {
-            return endPoint.Address.ToString();
-        } else {
-            return "127.0.0.1";
-        }
-    }
-}
+//
+// static string LocalIPAddress() {
+//     using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0)) {
+//         socket.Connect("8.8.8.8", 65530);
+//         IPEndPoint? endPoint = socket.LocalEndPoint as IPEndPoint;
+//         if (endPoint != null) {
+//             return endPoint.Address.ToString();
+//         } else {
+//             return "127.0.0.1";
+//         }
+//     }
+// }
